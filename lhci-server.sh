a09bdc05224bd7c933fda86fff304f92022ec991
John David Garza <john.garza@utsa.edu>
2020-02-13 11:05:14 -0600

    adding global

--- lhci-server.sh
+++ lhci-server.sh
@@ -4,3 +4,4 @@ git commit --allow-empty -m "tick"
 git push
 lhci autorun --config=./provost-lighthouserc.json
 lhci autorun --config=./gs-lighthouserc.json
+lhci autorun --config=./global-lighthouserc.json
