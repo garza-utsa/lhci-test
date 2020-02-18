3887358ecb8eead817d606675819234622099a71
John David Garza <john.garza@utsa.edu>
2020-02-18 16:32:47 -0600

    reordering

--- lhci-server.sh
+++ lhci-server.sh
@@ -2,10 +2,10 @@
 
 git commit --allow-empty -m "tick"
 git push
-lhci autorun --config=./provost-lighthouserc.json
-lhci autorun --config=./gs-lighthouserc.json
-lhci autorun --config=./global-lighthouserc.json
-lhci autorun --config=./odl-lighthouserc.json
 lhci autorun --config=./dreamers-lighthouserc.json
-lhci autorun --config=./multi-lighthouserc.json
 lhci autorun --config=./future-lighthouserc.json
+lhci autorun --config=./global-lighthouserc.json
+lhci autorun --config=./gs-lighthouserc.json
+lhci autorun --config=./multi-lighthouserc.json
+lhci autorun --config=./odl-lighthouserc.json
+lhci autorun --config=./provost-lighthouserc.json
