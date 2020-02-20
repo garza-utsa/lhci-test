379f7abcaafdaa0191715801e890802ab588c5e1
John David Garza <john.garza@utsa.edu>
2020-02-20 16:17:05 -0600

    removing npm start and asserts

--- lhci-server.sh
+++ lhci-server.sh
@@ -2,10 +2,10 @@
 
 git commit --allow-empty -m "tick"
 git push
-lhci autorun --config=./dreamers-lighthouserc.json
-lhci autorun --config=./future-lighthouserc.json
-lhci autorun --config=./global-lighthouserc.json
-lhci autorun --config=./gs-lighthouserc.json
-lhci autorun --config=./multi-lighthouserc.json
-lhci autorun --config=./odl-lighthouserc.json
-lhci autorun --config=./provost-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./dreamers-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./future-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./global-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./gs-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./multi-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./odl-lighthouserc.json
+/usr/local/bin/lhci autorun --config=./provost-lighthouserc.json
