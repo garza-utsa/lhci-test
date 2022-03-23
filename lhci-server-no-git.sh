856a34f2f9bcd38cc01a9588ec9376aeb8d25fa7
John David Garza <john.garza@utsa.edu>
2022-03-23 14:09:59 -0500

    adding dls to the list

--- lhci-server-no-git.sh
+++ lhci-server-no-git.sh
@@ -2,14 +2,14 @@
 
 ##git commit --allow-empty -m "tick"
 ##git push
-/usr/local/bin/lhci autorun --config=./dls-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./dreamers-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./future-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./global-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./gs-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./online-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./odl-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./provost-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./mxcenter-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./my-lighthouserc.json
-/usr/local/bin/lhci autorun --config=./teaching-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./dls-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./dreamers-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./future-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./global-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./gs-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./online-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./odl-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./provost-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./mxcenter-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./my-lighthouserc.json
+/opt/local/bin/lhci autorun --config=./teaching-lighthouserc.json
--- lhci-server.sh
+++ lhci-server.sh
@@ -2,14 +2,14 @@
 
 /usr/bin/git commit --allow-empty -m "tick"
 /usr/bin/git push
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/dls-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/dreamers-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/future-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/global-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/gs-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/online-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/odl-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/my-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/provost-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/mxcenter-lighthouserc.json
-/usr/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/teaching-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/dls-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/dreamers-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/future-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/global-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/gs-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/online-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/odl-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/my-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/provost-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/mxcenter-lighthouserc.json
+/opt/local/bin/lhci autorun --config=/Users/rjq475/Development-vpaa/docker-lhci/lhci-test/teaching-lighthouserc.json
